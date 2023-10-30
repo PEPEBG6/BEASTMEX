f25b03593e0ebfe1973c1706dccc49f2c77b3fe7
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2023-10-30 17:40:47 -0600

    Nabvar
    Se agrego un nabvar para los modulos

--- BEASTMEX/resources/views/almacen.blade.php
+++ BEASTMEX/resources/views/almacen.blade.php
@@ -1,6 +1,11 @@
-@extends('layouts.plantilla')
-@section('titulo','Login')
+@extends('layouts.plantillaHome')
+
+@section('titulo','Almacen')
+
 @section('name')
+
 @section('contenido')
- <h1>holi</h1>
+
+ 
+
 @endsection
