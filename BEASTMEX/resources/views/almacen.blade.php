17938c41ca2195956591f6fdf60386b1b15f7e2d
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2023-10-30 18:54:23 -0600

    Pagina inicio Almacen
    Se agrego la pagina inicio del almacen

--- BEASTMEX/resources/views/almacen.blade.php
+++ BEASTMEX/resources/views/almacen.blade.php
@@ -5,7 +5,36 @@
 @section('name')
 
 @section('contenido')
+  
+    <link rel="stylesheet" href="css/stylesAlma.css">
+   
+  <p class="move-down fs-1 fw-bold">Productos</p>
 
- 
+  <div class="container col-md-11 p-4">
 
+        <div class="button-container">
+            <button type="button" class="btn btn-secondary" href="/">PDF</button>
+            <button type="button" class="btn btn-primary " href="/">+Registrar</button>
+        </div>
+      
+
+        <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">No. Serie</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Marca</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Costo</th>
+                        <th scope="col">Precio Venta</th>
+                        <th scope="col">Fecha ingreso</th>
+                        <th scope="col">Foto</th>
+                        <th scope="col">Opciones</th>
+                    </tr>
+                </thead>
+            
+        </table>
+
+  </div>
+    
 @endsection
