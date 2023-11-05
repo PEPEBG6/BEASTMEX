6ad14955890929f6ee35ac788669ed3f2d1dca05
Andres0319 <121038655@upq.edu.mx>
2023-11-04 22:35:22 -0600

    actualizacion

--- BEASTMEX/resources/views/compras.blade.php
+++ BEASTMEX/resources/views/compras.blade.php
@@ -14,7 +14,7 @@
 
         <div class="button-container">
             <button type="button" class="btn btn-secondary" href="/">PDF</button>
-            <a type="button" class="btn btn-primary " href="/regCom">+Registrar</a>
+            <a type="button" class="btn btn-primary " href="/regCom">Orden de Compra</a>
         </div>
       
 
@@ -24,6 +24,9 @@
                         <th scope="col">Productos</th>
                         <th scope="col">Existencia</th>
                         <th scope="col">Foto</th>
+                        <th scope="col">Altas y Bajas</th>
+
+                        
                     </tr>
                 </thead>
             
