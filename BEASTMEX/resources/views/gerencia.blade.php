34cfb80683e32a8ff8f07af092d2c7e7aaead9ea
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2023-11-28 15:20:17 -0600

    Diseño de las vistas

--- BEASTMEX/resources/views/gerencia.blade.php
+++ BEASTMEX/resources/views/gerencia.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.plantillaHome')
 
-@section('titulo','Almacen')
+@section('titulo','Gerenica')
 
 @section('name')
 
 @section('contenido')
   
-    <link rel="stylesheet" href="css/stylesGer.css"> 
+    <link rel="stylesheet" href="css/stylesAlma.css"> 
    
-  <p class="move-down fs-1 fw-bold">Gerencia</p>
+  <p class="move-down fs-1 fw-bold text-center text-white">Gerencia</p>
 
   <div class="container col-md-4 p-4">
 
